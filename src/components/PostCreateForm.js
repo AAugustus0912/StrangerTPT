6247280e61dc526840f33d7d2e3a24004ccd771b
Aaliyah Augustus <aaliyahaugustus@yahoo.com>
2022-10-31 21:14:50 -0500

    lst commit

--- src/components/PostCreateForm.js
+++ src/components/PostCreateForm.js
@@ -1,9 +1,16 @@
  import React from "react";
+ import { createPost } from "../api/api";
 
-const PostCreateForm = (props) => {
-return (<>
-<div>Post Create Form!</div>
-</>)
+const PostCreateForm = (token, setPost) => {
+
+return (<form className="ui form">
+<h3>Create Post!</h3>
+<label htmlFor="description">Description</label>
+<input type="text" placeholder="A description of Vacation Spot" required></input>
+<div>
+    
+</div>
+</form>)
 };
 
  export default PostCreateForm;
